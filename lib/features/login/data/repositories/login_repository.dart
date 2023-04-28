0ddb71013e3c8366f3082c16a39611cfc575d895
Muhammet <muhammetvural@anadolu.edu.tr>
2023-04-28 16:30:06 +0300

    repository yazıldı

---
+++ lib/features/login/data/repositories/login_repository.dart
@@ -0,0 +1,10 @@
+import 'package:api_login_with_dio/core/network_manager/network_manager.dart';
+import 'package:api_login_with_dio/features/login/data/model/login_model.dart';
+import 'package:dio/dio.dart';
+
+class LoginRepository{
+  late NetworkManager networkManager;
+   final dio = Dio();
+   Future<LoginModel?>loginCall({required String email, required String password})async{
+    var response = await dio.post(networkManager.networkManager + "login");}
+}
